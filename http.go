be198c0d0a2c70c5d6dd89f7f3877f93a3b82ab8
Loic Blot <loic.blot@unix-experience.fr>
2017-11-11 16:18:03 +0100

    Start http helpers

---
+++ http.go
@@ -0,0 +1,29 @@
+package easyhttp
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func ReadJsonRequest(payload io.ReadCloser, decodedPayload interface{}) bool {
+	decoder := json.NewDecoder(payload)
+	defer payload.Close()
+	if err := decoder.Decode(&decodedPayload); err != nil {
+		return false
+	}
+
+	return true
+}
+func WriteHTTPJsonResponse(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != err {
+		log.Fatal(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
