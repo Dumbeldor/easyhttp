fee932d99357f402e03381c0b35e77a92f48d602
Loic Blot <loic.blot@unix-experience.fr>
2017-11-13 12:23:08 +0100

    Add ErrorResponse type + GetJWTAuthHeader helper

--- http.go
+++ http.go
@@ -5,7 +5,12 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
 
 func ReadJsonRequest(payload io.ReadCloser, decodedPayload interface{}) bool {
 	decoder := json.NewDecoder(payload)
@@ -26,4 +31,25 @@ func WriteHTTPJsonResponse(w http.ResponseWriter, code int, payload interface{})
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
+}
+
+func GetJWTAuthHeader(r *http.Request) (*string, *ErrorResponse, int) {
+	authHeader := r.Header.Get("Authorization")
+	if len(authHeader) == 0 {
+		er := &ErrorResponse{Message: "Authorization header is missing."}
+		return nil, er, http.StatusBadRequest
+	}
+
+	ahSplited := strings.Split(authHeader, " ")
+	if len(ahSplited) != 2 {
+		er := &ErrorResponse{Message: "Malformed Authorization header."}
+		return nil, er, http.StatusBadRequest
+	}
+
+	if ahSplited[0] != "JWT" {
+		er := &ErrorResponse{Message: "Authorization header doesn't describe a JWT Authorization."}
+		return nil, er, http.StatusBadRequest
+	}
+
+	return &ahSplited[1], nil, 0
 }
